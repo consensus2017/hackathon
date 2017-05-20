8fcde3d32262925a12696854a0f4f5b954b8a8de
Nicholas Addison <nicholasaddison@Nicholass-MacBook-Pro-2.local>
2017-05-20 14:54:03 -0400

    blackbox contract

---
+++ contracts/blackbox.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.10;
+
+contract Blackbox {
+
+    enum LogType {Poximity, Speeding, NoIndication, RedLight}
+
+    event LogEvent(address observer, LogType, int8 Severity, string location, uint64 time, string carId);
+
+    function logEvent(LogType eventType, int8 severity, string location, uint64 time, string carId) {
+        
+        if (severity < 0 || severity > 5)
+        {
+            throw;
+        }
+        
+        LogEvent(msg.sender, eventType, severity, location, time, carId);
+    }
+}
